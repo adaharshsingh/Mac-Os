856383935dfb018a464d1c43f2072f41449adda7
Adarsh <Mr.aadarshkumarsingh@gmail.com>
2026-01-17 02:09:58 +0530

    fix

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -18,7 +18,7 @@ const renderText =(text, className,baseWeight =400)=>{
 )}
 
 const setTextHover=(container,type)=>{
-    if(!container)return;
+    if(!container)return()=>{};
     const letters = container.querySelectorAll('span');
     const {min,max,default:base}=FONT_WEIGHTS[type];
     const animatedLetters=(letter, weight ,duration =0.25)=>{
